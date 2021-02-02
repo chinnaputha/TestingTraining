3e835b98bab0abc383895c8915d12be3e20c5ee8
Chinna.Putha <chinnareddy.putha@gmail.com>
2021-02-02 08:07:12 +0530

    added extra method

--- src/com/training/corejava/ArithOperations.java
+++ src/com/training/corejava/ArithOperations.java
@@ -22,11 +22,22 @@ public class ArithOperations {
 		System.out.println("a%b  --->"+(a%b));
 	}
 	
+	public void arithTest2() {
+		int a=8,b=4;
+		System.out.println("a+b  --->"+(a+b));
+		System.out.println("a-b  --->"+(a-b));
+		System.out.println("a*b  --->"+(a*b));
+		System.out.println("a/b  --->"+(a/b));
+		System.out.println("a%b  --->"+(a%b));
+	}
+	
 	
 	public static void main(String[] args) {
 		
+		//Object creation
 		ArithOperations ao = new ArithOperations();
-		ao.arithTest();
+		ao.arithTest();//call the method using object name i.e. ao
+		ao.arithTest2();//call the method using object name i.e. ao
 		
 		
 	}
