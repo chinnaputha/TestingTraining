53333f8991a872c4c69cd2ebf3e91db04ae58b26
Chinna.Putha <chinnareddy.putha@gmail.com>
2021-03-14 09:21:38 +0530

    sauce configuration code

--- src/com/training/automation/TestBaseUtil.java
+++ src/com/training/automation/TestBaseUtil.java
@@ -2,6 +2,7 @@ package com.training.automation;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,6 +14,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestBaseUtil {
 
@@ -59,6 +63,99 @@ public class TestBaseUtil {
 		// File destFile = new File("screenshots\\"+this.getClass().getSimpleName() +
 		// getTodaysDate() + ".png");
 		FileUtils.copyFile(srcFile, destFile);
+
+	}
+	
+	/**
+	 * Method to connect saucelabs
+	 * @param os
+	 * @param browser
+	 * @throws Exception
+	 */
+	public void runOnSauceLabsFromLocal(String os, String browser) throws Exception {
+
+		String username = "autotestjavaselenium";
+		String access_key = "3cab11e15-b5f0-4f2c-8afa-1152ec69f3d3";
+	    String url = "https://" + username + ":" + access_key + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+
+		//String url ="https://autotestjavaselenium:cab11e15-b5f0-4f2c-8afa-1152ec69f3d3@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+		DesiredCapabilities caps = null;
+
+		if (os.equalsIgnoreCase("Windows")) {
+ 
+			if (browser.equalsIgnoreCase("Chrome")) {
+				caps = DesiredCapabilities.chrome();
+				caps.setCapability("platform", "Windows 10"); 
+				caps.setCapability("version", "88.0");
+				caps.setCapability("name", "MySauceTest");
+				
+			}
+
+			if (browser.equalsIgnoreCase("FF")) {
+				caps = DesiredCapabilities.firefox();
+				caps.setCapability("platform", "Windows 7");
+				caps.setCapability("version", "74.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
+			}
+
+			if (browser.equalsIgnoreCase("IE")) {
+				caps = DesiredCapabilities.internetExplorer();
+				caps.setCapability("platform", "Windows 8.1");
+				caps.setCapability("version", "11.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
+			}
+
+		}
+
+		if (os.equalsIgnoreCase("Mac")) {
+
+			if (browser.equalsIgnoreCase("Chrome")) {
+
+				caps = DesiredCapabilities.chrome();
+				caps.setCapability("platform", "OS X 10.11");
+				caps.setCapability("version", "48.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
+			}
+
+			if (browser.equalsIgnoreCase("FF")) {
+
+				caps = DesiredCapabilities.firefox();
+				caps.setCapability("platform", "OS X 10.11");
+				caps.setCapability("version", "44.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
+			}
+
+			if (browser.equalsIgnoreCase("Safari")) {
+
+				caps = DesiredCapabilities.safari();
+				caps.setCapability("platform", "OS X 10.11");
+				caps.setCapability("version", "9.0");
+				caps.setCapability("name", this.getClass().getSimpleName());
+			}
+		}
+
+		if (os.equalsIgnoreCase("iOS")) {
+
+			caps = DesiredCapabilities.iphone();
+			caps.setCapability("platform", "OS X 10.10");
+			caps.setCapability("version", "9.2");
+			caps.setCapability("deviceName", "iPhone 6");
+			caps.setCapability("deviceOrientation", "portrait");
+			caps.setCapability("name", this.getClass().getSimpleName());
+		}
+
+		if (os.equalsIgnoreCase("Android")) {
+
+			caps = DesiredCapabilities.android();
+			caps.setCapability("platform", "Linux");
+			caps.setCapability("version", "5.1");
+			caps.setCapability("deviceName", "Android Emulator");
+			caps.setCapability("deviceType", "phone");
+			caps.setCapability("deviceOrientation", "portrait");
+			caps.setCapability("name", this.getClass().getSimpleName());
+		}
+
+		driver = new RemoteWebDriver(new URL(url), caps);
 
 	}
 
